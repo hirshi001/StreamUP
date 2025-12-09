1bdc016ce2bbc2fcc1cf609d206a39d1bb3ffe86
Hrishikesh Ingle <hningle@wisc.edu>
2025-12-08 18:23:53 -0600

    update read/write usage. They now return how many bytes were read/written. Update tests to reflect this

--- StreamUPLib/StreamUP/Data/include/Data/buffer/ArrayBuffer.h
+++ StreamUPLib/StreamUP/Data/include/Data/buffer/ArrayBuffer.h
@@ -3,159 +3,101 @@
 
 namespace SUP::BufferUtil
 {
-
 template<typename T>
-void writeInt(uint8_t* data, T value)
-requires std::is_integral_v<T>
+size_t writeInt(uint8_t *data, T value)
+    requires std::is_integral_v<T>
 {
-
     constexpr size_t N = sizeof(T);
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i)
+    {
         data[i] = static_cast<uint8_t>(value >> (8 * (N - 1 - i))); // MSB first
     }
+    return N;
 }
 
 template<typename T>
-T readInt(const uint8_t* data)
-requires std::is_integral_v<T>
+size_t readInt(const uint8_t *data, T &value)
+    requires std::is_integral_v<T>
 {
-    T value = 0;
+    value = 0;
     constexpr size_t N = sizeof(T);
 
-    for (size_t i = 0; i < N; ++i) {
+    for (size_t i = 0; i < N; ++i)
+    {
         value |= static_cast<T>(data[i]) << (8 * (N - 1 - i));
     }
-
-    return value;
-}
-
-
-template<typename T>
-void writeIntUpdate(uint8_t*& data, T value)
-requires std::is_integral_v<T>
-{
-    writeInt(data, value);
-    data += sizeof(T);
+    return N;
 }
 
 template<typename T>
-T readIntUpdate(const uint8_t*& data)
-requires std::is_integral_v<T>
-{
-    T value = readInt<T>(data);
-    data += sizeof(T);
-    return value;
-}
-
-template<typename T>
-void writeFloat(uint8_t* data, T value)
-requires std::is_floating_point_v<T>
+size_t writeFloat(uint8_t *data, T value)
+    requires std::is_floating_point_v<T>
 {
     // Copy bytes, then ensure big-endian
-    uint8_t tmp[sizeof(T)];
-    std::memcpy(tmp, &value, sizeof(T));
+    constexpr size_t N = sizeof(T);
+
+    uint8_t tmp[N];
+    std::memcpy(tmp, &value, N);
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
     // Reverse bytes for network byte order
-    for (size_t i = 0; i < sizeof(T); ++i) {
-        data[i] = tmp[sizeof(T) - 1 - i];
+    for (size_t i = 0; i < N; ++i)
+    {
+        data[i] = tmp[N - 1 - i];
     }
 #else
-    std::memcpy(data, tmp, sizeof(T));
+    std::memcpy(data, tmp, N);
 #endif
-    data += sizeof(T);
+    return N;
 }
 
 
 template<typename T>
-T readFloat(const uint8_t* data)
-requires std::is_floating_point_v<T>
+size_t readFloat(const uint8_t *data, T &value)
+    requires std::is_floating_point_v<T>
 {
-    uint8_t tmp[sizeof(T)];
+    constexpr size_t N = sizeof(T);
+    uint8_t tmp[N];
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
     // Reverse from network byte order to native
-    for (size_t i = 0; i < sizeof(T); ++i) {
-        tmp[i] = data[sizeof(T) - 1 - i];
+    for (size_t i = 0; i < N; ++i)
+    {
+        tmp[i] = data[N - 1 - i];
     }
 #else
-    std::memcpy(tmp, data, sizeof(T));
+    std::memcpy(tmp, data, N);
 #endif
 
-    T value;
-    std::memcpy(&value, tmp, sizeof(T));
-    return value;
-}
-
-
-template<typename T>
-void writeFloatUpdate(uint8_t*& data, T value)
-requires std::is_floating_point_v<T>
-{
-    writeFloat<T>(data, value);
-    data += sizeof(T);
+    value = 0;
+    std::memcpy(&value, tmp, N);
+    return N;
 }
 
 template<typename T>
-T readFloatUpdate(const uint8_t*& data)
-requires std::is_floating_point_v<T>
+size_t write(uint8_t *data, T value)
+    requires std::is_arithmetic_v<T>
 {
-    T value = readFloat<T>(data);
-    data += sizeof(T);
-    return value;
-}
-
-template<typename T>
-void write(uint8_t* data, T value)
-requires std::is_arithmetic_v<T>
-{
-    if constexpr (std::is_integral_v<T>) {
-        writeInt<T>(data, value);
-    }
-    else if constexpr (std::is_floating_point_v<T>) {
-        writeFLoat<T>(data, value);
+    if constexpr (std::is_integral_v<T>)
+    {
+        return writeInt<T>(data, value);
+    } else if constexpr (std::is_floating_point_v<T>)
+    {
+        return writeFloat<T>(data, value);
     }
 }
 
 
 template<typename T>
-T read(const uint8_t* data, T value)
-requires std::is_arithmetic_v<T>
+size_t read(const uint8_t *data, T& value)
+    requires std::is_arithmetic_v<T>
 {
-    if constexpr (std::is_integral_v<T>) {
+    if constexpr (std::is_integral_v<T>)
+    {
         return readInt<T>(data, value);
-    }
-    else if constexpr (std::is_floating_point_v<T>) {
+    } else if constexpr (std::is_floating_point_v<T>)
+    {
         return readFloat<T>(data, value);
     }
 }
-
-template<typename T>
-void writeUpdate(uint8_t*& data, T value)
-requires std::is_arithmetic_v<T>
-{
-    if constexpr (std::is_integral_v<T>) {
-        writeIntUpdate<T>(data, value);
-    }
-    else if constexpr (std::is_floating_point_v<T>) {
-        writeFloatUpdate<T>(data, value);
-    }
 }
-
-
-template<typename T>
-T readUpdate(const uint8_t*& data)
-requires std::is_arithmetic_v<T>
-{
-    if constexpr (std::is_integral_v<T>) {
-        return readIntUpdate<T>(data);
-    }
-    else if constexpr (std::is_floating_point_v<T>) {
-        return readFloatUpdate<T>(data);
-    }
-}
-
-
-}
-
-
--- StreamUPLib/StreamUP/Data/test/BufferTest.cpp
+++ StreamUPLib/StreamUP/Data/test/BufferTest.cpp
@@ -10,157 +10,70 @@
 using namespace SUP;
 using namespace SUP::BufferUtil;
 
-BOOST_AUTO_TEST_CASE(BufferUIntTest)
+BOOST_AUTO_TEST_CASE(BufferTest)
 {
     std::vector<uint8_t> array;
     array.resize(1024);
 
-    uint8_t a = 123;
-    uint16_t b = 4567;
-    uint32_t c = 123456789;
-    uint64_t d = 123456789123456789;
-    uint8_t e = 111;
+    uint8_t wA1 = 123, rA1;
+    uint16_t wB1 = 4567, rB1;
+    uint32_t wC1 = 123456789, rC1;
+    uint64_t wD1 = 123456789123456789, rD1;
+    uint8_t wE1 = 111, rE1;
+
+
+    int8_t wA2 = -123, rA2;
+    int16_t wB2 = 4567, rB2;
+    int32_t wC2 = -123456789, rC2;
+    int64_t wD2 = 123456789123456789, rD2;
+    int8_t wE2 = 111, rE2;
+
+    float wA3 = 135.531513, rA3;
+    double wB3 = 5398647.421, rB3;
+
 
     uint8_t *data = array.data();
-    writeInt<uint8_t>(data, a);
-    data += sizeof(uint8_t);
-    writeInt<uint16_t>(data, b);
-    data += sizeof(uint16_t);
-    writeInt<uint32_t>(data, c);
-    data += sizeof(uint32_t);
-    writeInt<uint64_t>(data, d);
-    data += sizeof(uint64_t);
-    writeInt<uint8_t>(data, e);
-    data += sizeof(uint8_t);
+    data += write<uint8_t>(data, wA1);
+    data += write<uint16_t>(data, wB1);
+    data += write<uint32_t>(data, wC1);
+    data += write<uint64_t>(data, wD1);
+    data += write<uint8_t>(data, wE1);
+
+    data += write<int8_t>(data, wA2);
+    data += write<int16_t>(data, wB2);
+    data += write<int32_t>(data, wC2);
+    data += write<int64_t>(data, wD2);
+    data += write<int8_t>(data, wE2);
+
+    data += write<float>(data, wA3);
+    data += write<double>(data, wB3);
+
 
     data = array.data();
-    BOOST_REQUIRE_EQUAL(readInt<uint8_t>(data), a);
-    data += sizeof(uint8_t);
-    BOOST_REQUIRE_EQUAL(readInt<uint16_t>(data), b);
-    data += sizeof(uint16_t);
-    BOOST_REQUIRE_EQUAL(readInt<uint32_t>(data), c);
-    data += sizeof(uint32_t);
-    BOOST_REQUIRE_EQUAL(readInt<uint64_t>(data), d);
-    data += sizeof(uint64_t);
-    BOOST_REQUIRE_EQUAL(readInt<uint8_t>(data), e);
-    data += sizeof(uint8_t);
-}
+    data += read<uint8_t>(data, rA1);
+    BOOST_REQUIRE_EQUAL(wA1, rA1);
+    data += read<uint16_t>(data, rB1);
+    BOOST_REQUIRE_EQUAL(wB1, rB1);
+    data += read<uint32_t>(data, rC1);
+    BOOST_REQUIRE_EQUAL(wC1, rC1);
+    data += read<uint64_t>(data, rD1);
+    BOOST_REQUIRE_EQUAL(wD1, rD1);
+    data += read<uint8_t>(data, rE1);
+    BOOST_REQUIRE_EQUAL(wE1, rE1);
 
-BOOST_AUTO_TEST_CASE(BufferIntTest)
-{
-    std::vector<uint8_t> array;
-    array.resize(1024);
+    data += read<int8_t>(data, rA2);
+    BOOST_REQUIRE_EQUAL(wA2, rA2);
+    data += read<int16_t>(data, rB2);
+    BOOST_REQUIRE_EQUAL(wB2, rB2);
+    data += read<int32_t>(data, rC2);
+    BOOST_REQUIRE_EQUAL(wC2, rC2);
+    data += read<int64_t>(data, rD2);
+    BOOST_REQUIRE_EQUAL(wD2, rD2);
+    data += read<int8_t>(data, rE2);
+    BOOST_REQUIRE_EQUAL(wE2, rE2);
 
-    int8_t a = -123;
-    int16_t b = 4567;
-    int32_t c = -123456789;
-    int64_t d = 123456789123456789;
-    int8_t e = -111;
-
-    uint8_t *data = array.data();
-    writeInt<int8_t>(data, a);
-    data += sizeof(int8_t);
-    writeInt<int16_t>(data, b);
-    data += sizeof(int16_t);
-    writeInt<int32_t>(data, c);
-    data += sizeof(int32_t);
-    writeInt<int64_t>(data, d);
-    data += sizeof(int64_t);
-    writeInt<int8_t>(data, e);
-    data += sizeof(int8_t);
-
-    data = array.data();
-    BOOST_REQUIRE_EQUAL(readInt<int8_t>(data), a);
-    data += sizeof(int8_t);
-    BOOST_REQUIRE_EQUAL(readInt<int16_t>(data), b);
-    data += sizeof(int16_t);
-    BOOST_REQUIRE_EQUAL(readInt<int32_t>(data), c);
-    data += sizeof(int32_t);
-    BOOST_REQUIRE_EQUAL(readInt<int64_t>(data), d);
-    data += sizeof(int64_t);
-    BOOST_REQUIRE_EQUAL(readInt<int8_t>(data), e);
-    data += sizeof(int8_t);
-}
-
-
-BOOST_AUTO_TEST_CASE(BufferUpdateTest)
-{
-    std::vector<uint8_t> array;
-    array.resize(1024);
-
-    uint8_t a = 123;
-    uint16_t b = 4567;
-    uint32_t c = 123456789;
-    uint64_t d = 123456789123456789;
-    uint8_t e = 111;
-
-    uint8_t *data = array.data();
-    writeIntUpdate<uint8_t>(data, a);
-    writeIntUpdate<uint16_t>(data, b);
-    writeIntUpdate<uint32_t>(data, c);
-    writeIntUpdate<uint64_t>(data, d);
-    writeIntUpdate<uint8_t>(data, e);
-
-    data = array.data();
-    BOOST_REQUIRE_EQUAL(readInt<uint8_t>(data), a);
-    data += sizeof(uint8_t);
-    BOOST_REQUIRE_EQUAL(readInt<uint16_t>(data), b);
-    data += sizeof(uint16_t);
-    BOOST_REQUIRE_EQUAL(readInt<uint32_t>(data), c);
-    data += sizeof(uint32_t);
-    BOOST_REQUIRE_EQUAL(readInt<uint64_t>(data), d);
-    data += sizeof(uint64_t);
-    BOOST_REQUIRE_EQUAL(readInt<uint8_t>(data), e);
-    data += sizeof(uint8_t);
-
-    const uint8_t* rptr = array.data();
-    BOOST_REQUIRE_EQUAL(readIntUpdate<uint8_t>(rptr), a);
-    BOOST_REQUIRE_EQUAL(readIntUpdate<uint16_t>(rptr), b);
-    BOOST_REQUIRE_EQUAL(readIntUpdate<uint32_t>(rptr), c);
-    BOOST_REQUIRE_EQUAL(readIntUpdate<uint64_t>(rptr), d);
-    BOOST_REQUIRE_EQUAL(readIntUpdate<uint8_t>(rptr), e);
-}
-
-
-BOOST_AUTO_TEST_CASE(BufferAllTypesTest)
-{
-    std::vector<uint8_t> array;
-    array.resize(1024);
-
-    uint8_t a = 123;
-    uint16_t b = 4567;
-    uint32_t c = 123456789;
-    uint64_t d = 123456789123456789;
-    int8_t e = -123;
-    int16_t f = 4567;
-    int32_t g = -123456789;
-    int64_t h = 123456789123456789;
-    float i = 127.43F;
-    double j = 1356245138426594.12452364;
-
-
-
-    uint8_t *data = array.data();
-    writeUpdate(data, a);
-    writeUpdate(data, b);
-    writeUpdate(data, c);
-    writeUpdate(data, d);
-    writeUpdate(data, e);
-    writeUpdate(data, f);
-    writeUpdate(data, g);
-    writeUpdate(data, h);
-    writeUpdate(data, i);
-    writeUpdate(data, j);
-
-    const uint8_t* rptr = array.data();
-    BOOST_REQUIRE_EQUAL(readUpdate<uint8_t>(rptr), a);
-    BOOST_REQUIRE_EQUAL(readUpdate<uint16_t>(rptr), b);
-    BOOST_REQUIRE_EQUAL(readUpdate<uint32_t>(rptr), c);
-    BOOST_REQUIRE_EQUAL(readUpdate<uint64_t>(rptr), d);
-    BOOST_REQUIRE_EQUAL(readUpdate<int8_t>(rptr), e);
-    BOOST_REQUIRE_EQUAL(readUpdate<int16_t>(rptr), f);
-    BOOST_REQUIRE_EQUAL(readUpdate<int32_t>(rptr), g);
-    BOOST_REQUIRE_EQUAL(readUpdate<int64_t>(rptr), h);
-    BOOST_REQUIRE_EQUAL(readUpdate<float>(rptr), i);
-    BOOST_REQUIRE_EQUAL(readUpdate<double>(rptr), j);
+    data += read<float>(data, rA3);
+    BOOST_REQUIRE_EQUAL(wA3, rA3);
+    data += read<double>(data, rB3);
+    BOOST_REQUIRE_EQUAL(wB3, rB3);
 }
